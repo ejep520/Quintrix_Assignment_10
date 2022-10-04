5bc19cc1d99bc54f0c24aaa0c0ecd85401358473
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-10-03 18:34:43 -0700

    Began adding in the H2 DB for user tracking

---
+++ src/main/java/com/quintrix/jepsen/erik/tenth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+/*
+ * package com.quintrix.jepsen.erik.tenth.repository;
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository; import
+ * org.springframework.stereotype.Repository; import com.quintrix.jepsen.erik.tenth.model.TenthUser;
+ * 
+ * @Repository public interface UserRepository extends JpaRepository<TenthUser, Integer> {
+ * 
+ * }
+ */
